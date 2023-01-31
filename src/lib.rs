bc93f6019689f9b82e6b2e0b7fd66a4f85628899
Harish R <dev@rhrish.com>
2023-01-31 01:54:54 +0100

    Basic structure and CLI options updated

---
+++ src/lib.rs
@@ -0,0 +1,94 @@
+use clap::{App,Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Harish <dev@rhrish.com>")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("lines")
+            .short("n")
+            .long("lines")
+            .value_name("LINES")
+            .help("Number of lines")
+            .default_value("10")
+            .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("bytes")
+            .short("c")
+            .long("bytes")
+            .value_name("BYTES")
+            .conflicts_with("lines")
+            .help("Number of bytes")
+            .takes_value(true)
+            .required(false)
+        )
+        .arg(
+            Arg::with_name("FILE")
+            .help("Input file(s)")
+            .default_value("-")
+            .takes_value(true)
+            .multiple(true)
+        )
+        .get_matches();
+    
+    let lines = matches
+                .value_of("lines")
+                .map(parse_positive_int)
+                .transpose()
+                .map_err(|e| format!("illegal line count -- {}", e))?;
+    
+    let bytes = matches
+                .value_of("bytes")
+                .map(parse_positive_int)
+                .transpose()
+                .map_err(|e|format!("illegal byte count -- {}", e))?;
+
+    Ok( Config {
+            files: matches.values_of_lossy("FILE").unwrap(),
+            lines: lines.unwrap(),
+            bytes: bytes,
+        })
+
+}
+
+pub fn run(config: Config) -> MyResult<()>{
+    println!("{:?}", config);
+    Ok(())
+}
+
+fn parse_positive_int(val :&str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n>0  => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // Test a regular positive number is not an error
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // Test a random string is an error
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // Test zero is an error
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
